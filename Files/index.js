1a06c5ff34473e5bacf163e09baea43a7b99e322
Yussef Rafat <yussefrafat5908@gmail.com>
2021-07-23 20:48:36 -0400

    questions input

--- Files/index.js
+++ Files/index.js
@@ -3,7 +3,48 @@ const inquirer = require('inquirer');
 const generatorMarkdown = require("./utils/generateMarkdown")
 const fs = require("fs");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "What is your GitHub username ?",
+        name: "UserName",
+    },
+    {
+        type: "input",
+        message: "What is your email?",
+        name: "Email",
+    },
+    {
+        type: "input",
+        message: "What is the title for your project?",
+        name: "Title",
+    },
+    {
+        type: "input",
+        message: "Please give description of your project.",
+        name: "Description",
+    },
+    {
+        type: "input",
+        message: "What necessary dependencies of your project.",
+        name: "Installation",
+    },
+    {
+        type: "input",
+        message: "What is this app used for?",
+        name: "License",
+    },
+    {
+        type: "input",
+        message: "What license was used for this README?",
+        name: "Contributor",
+    },
+    {
+        type: "input",
+        message: "What command do you use to test this App?",
+        name: "Test",
+    },
+];
  
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
